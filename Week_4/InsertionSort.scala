63e544ea865c27d563c20a4e0ccc858ac7ce075d
Tim Wilkens <timwilkensf1@gmail.com>
2014-05-26 13:49:14 -0700

    Insertion Sort implementation.

---
+++ Week_4/InsertionSort.scala
@@ -0,0 +1,13 @@
+def isort(xs: List[Int]): List[Int] = xs match {
+  case List() => List()
+  case y :: ys => insert(y, isort(ys))
+}
+
+def insert(x: Int, xs: List[Int]): List[Int] = xs match {
+  case List() => List(x)
+  case y :: ys => if (x <= y) x :: xs else y :: insert(x, ys)
+}
+
+val unsorted = List(10, 1, 9, 2, 8, 3)
+val sorted = isort(unsorted)
+println(sorted)
